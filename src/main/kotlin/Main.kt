f0cc6df85df840cda5990313b0cf28f5b706be3d
Evgeniy Cherkasov <evgenii.cherkasov@yandex.ru>
2024-01-28 22:47:59 +0500

    End

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,43 @@
 package ru.netology
 
+class Comments(val comment: String)
+data class Post(val id: Int,
+           val date: Int,
+           val text: String,
+           val friendsOnly: Boolean,
+           val comments: Comments,
+           val canPin: Boolean,
+           val canDelete: Boolean,
+           val canEdit: Boolean,
+           val isPinned: Boolean,
+           val isFavorite: Boolean)
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var id = posts.size + 1
+
+    fun add(post: Post): Post  {
+        posts += post.copy(id = id)
+        return posts.last()
+    }
+
+    fun update (postUp: Post): Boolean {
+        var i = 0
+        while (i in posts.indices) {
+            if (posts[i].id == postUp.id) {
+                posts[i] = postUp
+                return true
+            }
+           i++
+        }
+        return false
+    }
+    fun last(): Post {
+        return posts.last()
+    }
+}
+
+
+
 fun main() {
 }
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,37 @@
+import org.junit.Assert.*
+import org.junit.Test
+import ru.netology.Comments
+import ru.netology.Post
+import ru.netology.WallService
+
+class WallServiceTest {
+
+    @Test
+    fun addTest() {
+        val post = Post(1, 280124, "Hello", true, Comments("World"), true, true,
+            true, false, true)
+        WallService.add(post)
+        assertEquals(1, WallService.last().id)
+
+    }
+
+    @Test
+    fun updateTestTrue() {
+        Post(1, 280124, "Hello", true, Comments("World"), true, true,
+            true, false, true)
+        val post1 = Post(1, 280124, "Hello", true, Comments("World"), true, true,
+            true, false, true)
+
+        assertEquals(true,WallService.update(post1) )
+    }
+    @Test
+    fun updateTestFalse() {
+        Post(1, 280124, "Hello", true, Comments("World"), true, true,
+            true, false, true)
+        val post1 = Post(5, 280124, "Hello", true, Comments("World"), true, true,
+            true, false, true)
+
+        assertEquals(false,WallService.update(post1) )
+    }
+
+}
